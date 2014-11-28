66f6c0506f8b55eb836a4577de98b94cb9e953d0
stephaniecoleman <sproutwingsandfly@gmail.com>
2014-11-27 21:03:35 -0600

    Initial commit.

---
+++ bootstrap/php/include/settings.php
@@ -0,0 +1,20 @@
+<?php
+
+	// MailChimp
+	$listID = "LIST-ID";
+	$APIKey = "API-KEY";
+
+	// Database
+	$host = 'YOUR-HOST';
+	$username = 'YOUR-USER';
+	$password = 'YOUR-PASSWORD';
+	$database = 'TEMPLATE-DATABASE';
+	
+	// Contact
+	$to = 'plinksapp@gmail.com';
+    $subject = 'Subject here...';
+	
+	// Variant
+	$useMailChimp = "NO";
+
+?>
